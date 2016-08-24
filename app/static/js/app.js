7d4d660c6f1782d5bdab8bed568e409aa028ae3b
smhigley <sarahmhigley@gmail.com>
2016-08-24 11:37:08 -0700

    reasonable looking falling leaves

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -840,6 +840,40 @@
 	}
 }());
 
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+// main functions
+
 (function() {
   'use strict';
 
@@ -860,50 +894,161 @@
     this.world = document.createElement('div');
     this.leaves = [];
 
-    this.width = this.world.offsetWidth;
-    this.height = this.world.offsetHeight;
-
     this.options = {
-      numLeaves: 30,
-      windspeed: 0.2
+      numLeaves: 20,
+      wind: {
+        magnitude: 1.2,
+        maxSpeed: 8,
+        duration: 300,
+        start: 0,
+        speed: 0
+      },
     };
+
+    this.width = this.viewport.offsetWidth;
+    this.height = this.viewport.offsetHeight;
+
+    // animation helper
+    this.timer = 0;
+
+    this._resetLeaf = function(leaf) {
+
+      // place leaf towards the top left
+      leaf.x = this.width * 2 - Math.random()*this.width*1.75;
+      leaf.y = -10;
+      leaf.z = Math.random()*100 - 50;
+      if (leaf.x > this.width) {
+        leaf.x = this.width + 10;
+        leaf.y = Math.random()*this.height/2;
+      }
+      // at the start, the leaf can be anywhere
+      if (this.timer == 0) {
+        leaf.y = Math.random()*this.height;
+      }
+
+      // rotate around the y or z axis
+      leaf.rotation.speed = Math.random()*10;
+      var randomAxis = Math.random();
+      if (randomAxis > 0.5) {
+        leaf.rotation.axis = 'X';
+      } else if (randomAxis > 0.25) {
+        leaf.rotation.axis = 'Y';
+        leaf.rotation.x = Math.random()*180 + 90;
+      } else {
+        leaf.rotation.axis = 'Z';
+        leaf.rotation.x = Math.random()*360 - 180;
+        // looks weird if the rotation is too fast around this axis
+        leaf.rotation.speed = Math.random()*3;
+      }
+
+      // random speed
+      leaf.xSpeedVariation = Math.random() * 0.8 - 0.4;
+      leaf.ySpeed = Math.random() + 1.5;
+
+      return leaf;
+    }
+
+    this._updateLeaf = function(leaf) {
+      var leafWindSpeed = this.options.wind.speed(this.timer - this.options.wind.start, leaf.y);
+
+      var xSpeed = leafWindSpeed + leaf.xSpeedVariation;
+      leaf.x -= xSpeed;
+      leaf.y += leaf.ySpeed;
+      leaf.rotation.value += leaf.rotation.speed;
+
+      console.log("y:", leaf.y, "wind:", leafWindSpeed);
+
+      var t = 'translateX( ' + leaf.x + 'px ) translateY( ' + leaf.y + 'px ) translateZ( ' + leaf.z + 'px )  rotate' + leaf.rotation.axis + '( ' + leaf.rotation.value + 'deg )';
+      if (leaf.rotation.axis !== 'X') {
+        t += ' rotateX(' + leaf.rotation.x + 'deg)';
+      }
+      leaf.el.style.webkitTransform = t;
+      leaf.el.style.MozTransform = t;
+      leaf.el.style.oTransform = t;
+      leaf.el.style.transform = t;
+
+      // reset if out of view
+      if (leaf.x < -10 || leaf.y > this.height + 10) {
+        this._resetLeaf(leaf);
+      }
+    }
+
+    this._updateWind = function() {
+      // wind follows a sine curve: asin(b*time + c) + a
+      // where a = wind magnitude as a function of leaf position, b = wind.duration, c = offset
+
+      if (this.timer === 0 || this.timer > (this.options.wind.start + this.options.wind.duration)) {
+
+        this.options.wind.magnitude = Math.random() * this.options.wind.maxSpeed;
+        this.options.wind.duration = Math.random() * 300 + 100 // random gust duration between 100 and 400
+        this.options.wind.start = this.timer;
+
+        var screenHeight = this.height;
+
+        this.options.wind.speed = function(t, y) {
+          // should go from full wind speed at the top, to 1/2 speed at the bottom, using leaf Y
+          var a = this.magnitude/2 * (screenHeight - 2*y/3)/screenHeight;
+          return a * Math.sin(2*Math.PI/this.duration * t + (3 * Math.PI/2)) + a;
+        }
+      }
+    }
   }
 
   LeafScene.prototype.init = function() {
-    for (var i = 0; i < this.numLeaves; i++) {
-      var leaf = var leaf = {
+
+    for (var i = 0; i < this.options.numLeaves; i++) {
+      var leaf = {
         el: document.createElement('div'),
-        x: Math.random()*this.width,
-        y: Math.random()*this.height/2,
-        r: 0, // rotation
-        xSpeed: 0,
+        x: 0,
+        y: 0,
+        z: 0,
+        rotation: {
+          axis: 'X',
+          value: 0,
+          speed: 0,
+          x: 0
+        },
+        xSpeedVariation: 0,
         ySpeed: 0,
-        rSpeed: 0,
-        pathType: 1
+        path: {
+          type: 1,
+          start: 0,
+
+        }
       };
+      this._resetLeaf(leaf);
       this.leaves.push(leaf);
       this.world.appendChild(leaf.el);
     }
 
     this.world.className = 'leaf-scene';
     this.viewport.appendChild(this.world);
+
+    // set perspective
+    this.world.style.webkitPerspective = 400;
+    this.world.style.MozPerspective = 400;
+    this.world.style.oPerspective = 400;
+    this.world.style.perspective = 400;
   }
 
-  LeafScene.prototype.resetLeaf = function(leaf) {
-    var x, y, r;
-
-    // start offscreen, within a third of the top right corner
-    x = Math.random()*this.width*2/3 + (this.width*2/3);
-    y = -10;
-    if (x > this.width) {
-      x = this.width + 10;
-      y = Math.random()*this.height/3;
+  LeafScene.prototype.render = function() {
+    this._updateWind();
+    console.log("FRAME START");
+    for (var i = 0; i < this.leaves.length; i++) {
+      this._updateLeaf(this.leaves[i]);
     }
 
+    this.timer++;
 
-
-    return leaf;
+    requestAnimationFrame(this.render.bind(this));
   }
+
+  // start up leaf scene
+  var leafContainer = document.querySelector('.falling-leaves'),
+      leaves = new LeafScene(leafContainer);
+
+  leaves.init();
+  leaves.render();
 
 
 })();
